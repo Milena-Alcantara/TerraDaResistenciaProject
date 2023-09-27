3b234aeb00574a0ba76e50ef11febf0b146db226
Milena.Lopes.Mesquita.Alcântara <milena.alcantara@zup.com.br>
2023-09-27 14:08:56 -0300

    Inicializando o projeto.

---
+++ src/main/java/com/edu/terraDaResistencia/TerraDaResistenciaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edu.terraDaResistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraDaResistenciaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraDaResistenciaProjectApplication.class, args);
+	}
+
+}
